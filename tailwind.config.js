52e48911b42ad1b944061741a095798b88481a53
isamadafridi <isamadafridi@gmail.com>
2026-08-08 16:39:04 +0500

    font fixing

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ export default {
         border: 'rgba(255,255,255,0.08)',
       },
       fontFamily: {
-        heading: ['"Lora"', '"Georgia"', 'serif'],
+        heading: ['"Roboto"', 'sans-serif'],
         body: ['"Poppins"', 'sans-serif'],
       },
     },
